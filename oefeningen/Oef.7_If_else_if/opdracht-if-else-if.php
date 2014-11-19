33021c9910a2551c85c17d29b9cf0e975dcf2395
Glen Lauwers <glenlauwers@MacBook-Pro-van-Glen.local>
2014-11-19 11:38:40 +0100

    Oef 7

--- oefeningen/Oef.7_If_else_if/opdracht-if-else-if.php
+++ oefeningen/Oef.7_If_else_if/opdracht-if-else-if.php
@@ -1,12 +1,59 @@
+<?php
+	$getal		=	14;
+
+	if ($getal >= 0 && $getal < 10)
+	{
+		$antwoord	=	"Het getal ligt tussen 0 en 10";
+	}
+	elseif ($getal >= 10 && $getal < 20) 
+	{
+		$antwoord	=	"Het getal ligt tussen 10 en 20.";
+	}
+	elseif ($getal >= 20 && $getal < 30)
+	{
+		$antwoord	=	"Het getal ligt tussen 20 en 30.";
+	}
+	elseif ($getal >= 30 && $getal < 40) 
+	{
+		$antwoord	=	"Het getal ligt tussen 30 en 40.";
+	}
+	elseif ($getal >= 40 && $getal < 50) 
+	{
+		$antwoord	=	"Het getal ligt tussen 40 en 50.";
+	}	
+	elseif ($getal >= 50 && $getal < 60) 
+	{
+		$antwoord	=	"Het getal ligt tussen 50 en 60.";
+	}	
+	elseif ($getal >= 60 && $getal < 70) 
+	{
+		$antwoord	=	"Het getal ligt tussen 60 en 70.";
+	}
+	elseif ($getal >= 70 && $getal < 80) 
+	{
+		$antwoord	=	"Het getal ligt tussen 70 en 80.";
+	}
+	elseif ($getal>=80 && $getal <90)
+	{
+		$antwoord	=	"Het getal light tussen 80 en 90.";
+	}
+	elseif ($getal >= 90 && $getal < 100) 
+	{
+		$antwoord	=	"Het getal ligt tussen 90 en 100.";
+	}
+	else
+	{
+		$antwoord	=	"Het getal is hoger dan 100.";
+	}
+
+	$antwoord_omgek	=	strrev($antwoord)
+?>
 <!doctype html>
 <html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Opdracht if else if</title> 
-        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
-        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
-        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
     </head>
     <body class="web-backend-opdracht">
         
@@ -20,6 +67,10 @@
                 <li>Zorg er vervolgens voor dat de boodschap omgekeerd afgedrukt wordt, bv '03 ne 02 nessut tgil lateg teH'.</li>
             </ul>  
         
+        <p>Mijn getal is: <?= $getal ?></p>
+        <p><?= $antwoord ?></p>
+        <p><?= $antwoord_omgek ?></p>
+
         </section>
 
     </body>
