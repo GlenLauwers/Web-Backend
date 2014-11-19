f9c954d0feeab61f10b4c5221de1db6b523bdaa3
Glen Lauwers <glenlauwers@MacBook-Pro-van-Glen.local>
2014-11-19 11:48:55 +0100

    Oef 8

--- oefeningen/Oef.8_Switch/opdracht-switch.php
+++ oefeningen/Oef.8_Switch/opdracht-switch.php
@@ -1,12 +1,47 @@
+<?php
+    $getal  =   2;
+
+    switch ($getal) 
+    {
+        case "1":
+            $antwoord = "maandag";
+        break;
+
+        case "2":
+            $antwoord = "dinsdag";
+        break;
+
+        case "3":
+            $antwoord = "woensdag";
+        break;
+
+        case "4":
+            $antwoord = "donderdag";
+        break;
+
+        case "5":
+            $antwoord = "vrijdag";
+        break;
+
+        case "6":
+            $antwoord = "zaterdag";
+        break;
+
+        case "7":
+            $antwoord = "zondag";
+        break;
+        
+        default:
+         $antwoord = "Je hebt geen correcte nummer ingegeven. :-(";
+        break;
+    }
+?>
 <!doctype html>
 <html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Opdracht switch</title>
-        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
-        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
-        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
     </head>
     <body class="web-backend-opdracht">
         
@@ -19,6 +54,9 @@
                 <li>Maak een PHP-script dat aan de hand van een getal ( tussen 1 en 7 ) de bijhorende dag afprint in kleine letters (geen hoofdletters!)</li>
                 <li>Maak gebruik van een switch en probeer alles te herschrijven i.p.v. te kopiëren.</li>
             </ul>  
+
+            <p>Mijn getal: <?= $getal ?></p>
+            <p><?=$antwoord ?></p>
 
         </section>
 
