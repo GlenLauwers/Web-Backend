86e6ac2e323cd4fb967cf86d9e5226110634e927
Glen Lauwers <glenlauwers@MacBook-Pro-van-Glen.local>
2014-11-22 20:02:03 +0100

    foreache

---
+++ oefeningen/5.Looping_statements/Oef3.foreach/opdracht-looping-statements-foreach(Deel2).php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht foreach</title>
+
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+            <h1 class="extra">Opdracht foreach: deel 2</h1>
+
+            <ul>
+                <li>Maak een HTML-document met een PHP code-block</li> 
+
+                <li>Maak een variabele <code>$tekst</code> met waarde (volledige inhoud van text-file.txt)</li>
+
+                <li>Analyseer hoe vaak elke letter van het alfabet voorkomt in de tekst (enkel de letters uit het alfabet, geen onderscheid tussen hoofdletters en kleine letters)</li>
+
+                <li>Toon de resultaten op het scherm, bv.
+                    
+                    <div class="facade-minimal" data-url="http://www.app.local/index.php">
+                        
+                        <ul>
+                            <li>E x 265</li>
+                            <li>A x 245</li>
+                            <li>...</li>
+                        </ul>
+
+                    </div>
+                    
+                    <p class="tip">Doe wat opzoekingswerk vooraleer je aan deze opdracht begint.</p>
+                </li>
+            </ul> 
+
+        </section>
+      
+
+    </body>
+</html>
