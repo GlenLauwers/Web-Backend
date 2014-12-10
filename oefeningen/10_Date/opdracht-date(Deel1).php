c2ec27e1ce6f0bd3d31a3918145be746d8388512
Glen Lauwers <glenlauwers@MacBook-Pro-van-Glen.local>
2014-12-10 10:50:06 +0100

    oef. date

---
+++ oefeningen/10_Date/opdracht-date(Deel1).php
@@ -0,0 +1,29 @@
+<?php
+    $timestamp      =   mktime (22, 35, 25, 1, 21, 1904);
+    //var_dump($timestamp);
+    
+    $datum          =   date ('d F Y, g:i:s a' , $timestamp);
+    //var_dump($datum);
+
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Oplossing date: deel 1</title>
+    </head>
+    <body class="web-backend-opdracht">
+        <section class="body">
+            <h1>Oplossing date: deel 1</h1>
+
+            <h2>Engels</h2>
+            <ul>
+                <li>Timestamp: <?= $timestamp ?></li>
+                <li>Date: <?= $datum ?></li>
+            </ul>
+        </section>
+    </body>
+</html>
---
+++ oefeningen/10_Date/opdracht-date(Deel2).php
@@ -0,0 +1,45 @@
+<?php
+
+    $timestamp      =   mktime (22, 35, 25, 1, 21, 1904);
+    //var_dump($timestamp);
+
+    $datum_engels          =   date ('d F Y, g:i:s a' , $timestamp);
+    //var_dump($datum);
+
+    setlocale(LC_ALL, 'nl_NL');
+
+    $datum_nl   =    strftime("%d %B %Y, %H:%M:%S", $timestamp);
+    //var_dump($datum_nl);
+
+
+
+?>
+
+<!doctype html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Oplossing date: deel 2</title>
+    </head>
+
+    <body>
+        <section class="body">
+
+            <h1>Oplossing date: deel 2</h1>
+
+            <h2>Engels</h2>           
+            <ul>
+                <li>Timestamp: <?= $timestamp ?></li>
+                <li>Date: <?= $datum_engels ?></li>
+            </ul>
+
+            <h2>Nederlands</h2>
+            <ul>
+                <li>Datum: <?= $datum_nl ?></li>
+            </ul>
+
+        </section>
+    </body>
+</html>
