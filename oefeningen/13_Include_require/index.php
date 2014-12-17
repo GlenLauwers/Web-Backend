b82b6b637ae160eb22105eccf07f92891fa048b1
Glen Lauwers <glenlauwers@mech-b106-09.syntraab.mechelen>
2014-12-17 14:01:18 +0100

    oef toegevoegd

---
+++ oefeningen/13_Include_require/index.php
@@ -0,0 +1,22 @@
+<?php
+    $artikels = array(
+                        array(  'titel' => 'PHP 8.4 gereleased',
+                                'inhoud'  => 'De nieuwste versie van PHP bevat uitbreidingen op oa. mind control-facades, in-browser 3d projecties en de API        voor drones.',
+                                'kernwoorden'  => 'Kernwoorden: new, PHP 8.4'),
+    
+                        array(  'titel' => 'Zymphovel framework',
+                                'inhoud'  => 'Wordt Zymphovel het nieuwste PHP-framework dat de wereld in een sneltempo zal veroveren?',
+                                'kernwoorden'  => 'Kernwoorden: frameworks'),
+    
+                        array(  'titel' => 'Rasmus Lerdorf vermist',
+                                'inhoud'  => 'De geestelijke vader van PHP Rasmus Lerdorf is sinds vorige week vermist nadat hij met zijn privé-jet richting        Ibiza vertrok.',
+                                'kernwoorden'  => 'Kernwoorden: bizar, rasmus lerdorf, feestneus'));
+
+
+    include 'view/header-partial.html';
+
+    include 'view/body-partial.html';
+
+    include 'view/footer-partial.html';
+?>
+
